1b351b4944121f28f7fdca32d0354acc08873240
Victor Sanchez <vicsmr94@gmail.com>
2019-11-08 19:43:10 +0100

    Changed code structure of GameView

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/GameView.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/views/GameView.java
@@ -6,30 +6,30 @@ import es.urjccode.mastercloudapps.adcs.draughts.models.Coordinate;
 
 public class GameView extends SubView {
 
-    private static final String[] COLORS = new String[]{"b", "n", " "};
+    private static final String[] COLORS = new String[] { "b", "n", " " };
 
-	public void write(Controller controller) {
+    public void write(Controller controller) {
         final int DIMENSION = controller.getDimension();
         this.writeNumbersLine(DIMENSION);
-        for(int i=0; i<DIMENSION; i++){
-            this.console.write((i+1)+"");
-            for(int j=0; j<DIMENSION; j++){
-                Color color = controller.getColor(new Coordinate(i,j));
-                if (color == null){
+        for (int i = 0; i < DIMENSION; i++) {
+            this.console.write((i + 1) + "");
+            for (int j = 0; j < DIMENSION; j++) {
+                Color color = controller.getColor(new Coordinate(i, j));
+                if (color == null) {
                     this.console.write(GameView.COLORS[2]);
                 } else {
                     this.console.write(GameView.COLORS[color.ordinal()]);
                 }
             }
-            this.console.writeln((i+1)+"");
+            this.console.writeln((i + 1) + "");
         }
         this.writeNumbersLine(DIMENSION);
-	}
+    }
 
     private void writeNumbersLine(final int DIMENSION) {
         this.console.write(" ");
-        for(int i=0; i<DIMENSION; i++){
-            this.console.write((i+1)+"");
+        for (int i = 0; i < DIMENSION; i++) {
+            this.console.write((i + 1) + "");
         }
         this.console.writeln();
     }
