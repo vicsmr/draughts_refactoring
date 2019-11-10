a4b1eef28f2cc6878997132cebb2782e8666b4cf
Victor Sanchez <vicsmr94@gmail.com>
2019-11-10 13:29:28 +0100

    Deleted commented code

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/PlayControllerTest.java
@@ -24,24 +24,4 @@ public class PlayControllerTest {
         assertNotNull(pieceTarget);
         assertEquals(pieceTarget.getColor(), Color.WHITE);
     }
-
-    // public void data(){
-    //     Coordinate[][] coordinates = new Coordinate[][] {
-    //         { new Coordinate(0,0), new Coordinate(0,0) },
-    //         { new Coordinate(0,0), new Coordinate(0,0), new Coordinate(0,0), new Coordinate(0,0) },
-    //         { new Coordinate(0,0), new Coordinate(0,0) },
-    //     };
-
-        // peon mueve una
-        // peon come una
-        // peon come varias
-        // peon se convierte en dama
-        // dama mueve una
-        // dama mueve varias
-        // dama come una
-        // dama come varias, atras
-
-    //}
-
-
 }
