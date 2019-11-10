8c8654e6f06c589a839bbdbe759368f179e86df8
Victor Sanchez <vicsmr94@gmail.com>
2019-11-10 14:54:19 +0100

    Added mocks to testGivenGameWhenMoveWithNotAdvancedThenError() method

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameTest.java
@@ -1,12 +1,33 @@
 package es.urjccode.mastercloudapps.adcs.draughts.models;
 
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class GameTest {
 
+    @Spy
+    private Board board;
+
+    @Spy
+    private Turn turn;
+
+    @InjectMocks
+    private Game gameMock;
+
     private Game game;
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     public GameTest() {
         game = new Game();
@@ -84,15 +105,11 @@ public class GameTest {
 
     @Test
     public void testGivenGameWhenMoveWithNotAdvancedThenError() {
-        assertEquals(Error.NOT_ADVANCED, this.advance(new Coordinate[][] { 
-            { new Coordinate(5, 6), new Coordinate(4, 7) },
-            { new Coordinate(2, 7), new Coordinate(3, 6) }, 
-            { new Coordinate(5, 4), new Coordinate(4, 3) },
-            { new Coordinate(1, 6), new Coordinate(2, 7) }, 
-            { new Coordinate(4, 3), new Coordinate(3, 4) },
-            { new Coordinate(0, 7), new Coordinate(1, 6) }, 
-            { new Coordinate(3, 4), new Coordinate(4, 5) }, 
-        }));        
+        Coordinate origin = new Coordinate(3, 4);
+        when(board.getColor(origin)).thenReturn(Color.WHITE);
+        when(board.isEmpty(origin)).thenReturn(false);
+        when(board.getPiece(origin)).thenReturn(new Piece(Color.WHITE));
+        assertEquals(Error.NOT_ADVANCED, gameMock.move(origin, new Coordinate(4, 5)));     
     }
 
     @Test
